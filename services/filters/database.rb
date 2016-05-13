9ecbe006167b8e36f0ddd16b9ee98f7afd422569
zaru <sakuraba@basicinc.jp>
2016-05-13 14:37:16 +0900

    fixed database search logic

--- services/filters/database.rb
+++ services/filters/database.rb
@@ -1,9 +1,12 @@
 module Filters
   class Database
     def self.call(word)
-      word = Word.where(word: word).first
-      return nil if word.nil?
-      "#{word.answer}\n#{word.url}"
+      new_word = Word.where(word: word).first
+      if new_word.nil?
+        new_word = Word.where('word LIKE ?', "%#{word}%").first
+      end
+      return nil if new_word.nil?
+      "#{new_word.answer}\n#{new_word.url}"
     end
   end
 end
